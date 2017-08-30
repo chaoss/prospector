1a4ca0dd9f1ecf62c5377bd59bd34f08b481317f
Santiago Dueñas <sduenas@bitergia.com>
2017-08-30 13:08:07 +0200

    Add staticfiles module to templates

--- wsgi/healthmeter/settings/common.py
+++ wsgi/healthmeter/settings/common.py
@@ -86,6 +86,8 @@ STATICFILES_FINDERS = (
     'compressor.finders.CompressorFinder',
 )
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'your secret key goes here'
 
