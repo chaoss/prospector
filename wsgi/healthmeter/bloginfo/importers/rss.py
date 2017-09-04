f7963c41a33aa208526514a11038cd19cee7e5e2
Santiago Dueñas <sduenas@bitergia.com>
2017-09-04 21:08:48 +0200

    Import blog posts using Perceval library

--- wsgi/healthmeter/bloginfo/importers/rss.py
+++ wsgi/healthmeter/bloginfo/importers/rss.py
@@ -1,12 +1,11 @@
 # Copyright 2017 Red Hat, Inc.
 # License: GPLv3 or any later version
 
-import time
-import datetime
-import dateutil.parser as dparser
-from email.utils import parseaddr
-import feedparser
 import logging
+from email.utils import parseaddr
+
+import grimoirelab.toolkit.datetime
+import perceval.backends.core.rss as rss_importer
 
 from healthmeter.hmeter_frontend.utils import get_participant
 
@@ -18,9 +17,12 @@ logger = logging.getLogger(__name__)
 class RSSImporter(BlogImporter):
     """Blog importer class that imports things from RSS feeds"""
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
     @staticmethod
-    def parse_date(time_tuple):
-        return datetime.datetime.fromtimestamp(time.mktime(time_tuple))
+    def parse_date(timestamp):
+        return grimoirelab.toolkit.datetime.unixtime_to_datetime(timestamp)
 
     @staticmethod
     def get_participant(author):
@@ -30,51 +32,31 @@ class RSSImporter(BlogImporter):
 
     @staticmethod
     def extract_timestamp(item):
-        for field in ('published', 'updated'):
-            try:
-                parsed_timestamp = item[field + '_parsed']
-                if parsed_timestamp:
-                    logger.info("Found timestamp in [%s] field: %s",
-                                field, parsed_timestamp)
-                    return RSSImporter.parse_date(parsed_timestamp)
-
-                else:
-                    timestamp = item[field]
-                    logger.warning("No parsed timestamp, from feedparser, "
-                                   "falling back on dateutil.parser: [%s]",
-                                   timestamp)
-
-                    try:
-                        return dparser.parse(timestamp)
-                    except ValueError:
-                        logger.warning("Invalid %s timestamp!", field)
-            except KeyError:
-                pass
-
-        logger.warning("Could not find timestamp, falling back to current time.")
-        return RSSImporter.parse_date(datetime.datetime.utcnow())
+        return RSSImporter.parse_date(item['updated_on'])
 
     @staticmethod
     def extract_author(item):
         try:
-            return RSSImporter.get_participant(item['author'])
-
+            return RSSImporter.get_participant(item['data']['author'])
         except KeyError:
             return None
 
     @staticmethod
     def extract_guid(item):
-        return (item.get('id') or item.get('link') or
-                (item.get('enclosures') and
-                 item['enclosures'][0]['href']) or
-                item.get('title'))
+        data = item['data']
+        return (data.get('id') or data.get('link') or
+                (data.get('enclosures') and data['enclosures'][0]['href']) or
+                data.get('title'))
 
     def _run(self):
         """Import all blog posts from aforementioned post"""
         logger.info("Importing blog posts from [%s] using feed URL [%s]",
                     self.object, self.object.rss_url)
-        parsedict = feedparser.parse(self.object.rss_url)
-        for item in parsedict['items']:
+
+        backend = rss_importer.RSS(self.object.rss_url)
+        items = backend.fetch()
+
+        for item in items:
             logger.info("Beginning to parse feed item")
 
             blogpost, created = self.object.posts.get_or_create(
@@ -82,20 +64,19 @@ class RSSImporter(BlogImporter):
                 defaults={
                     'timestamp': self.extract_timestamp(item),
                     'author': self.extract_author(item),
-                    'title': item['title']
+                    'title': item['data']['title']
                 })
 
             updated = created
 
             if created:
                 logger.info("Imported blog post [%s]", blogpost)
-
-            elif blogpost.title != item['title']:
+            elif blogpost.title != item['data']['title']:
                 updated = True
                 logger.info("Updating title of blog post [%s] from [%s] to "
                             "[%s]",
-                            blogpost, blogpost.title, item['title'])
-                blogpost.title = item['title']
+                            blogpost, blogpost.title, item['data']['title'])
+                blogpost.title = item['data']['title']
                 blogpost.save()
 
             if updated:
