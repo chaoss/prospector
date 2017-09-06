5c180513664956d594effd766068b7c2a106954e
Santiago Dueñas <sduenas@bitergia.com>
2017-09-06 14:31:17 +0200

    Remove deprecated TEMPLATES configuration from dev settings

--- wsgi/healthmeter/settings/dev.py
+++ wsgi/healthmeter/settings/dev.py
@@ -25,11 +25,6 @@ CACHES = {
     }
 }
 
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-
 MANAGERS = ADMINS
 COMPRESS_OFFLINE = False
 COMPRESS_ENABLED = False
